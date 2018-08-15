8bb8cf4856084a996d2e62616546b866ce380a3d
Simon Kiener <jugglinghobo@gmail.com>
2018-08-15 07:11:15 +0200

    removed paste error

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,7 +13,7 @@ set :ssh_options, {
 
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
-set :rbenv_roles, :all # default value<Paste>
+set :rbenv_roles, :all # default value
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
