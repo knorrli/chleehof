314233ec939fd06bf7ec7313198cda9275ca88a6
Simon Kiener <jugglinghobo@gmail.com>
2018-11-12 23:20:47 +0100

    globally disable autocomplete

--- public/admin/javascripts/application.js
+++ public/admin/javascripts/application.js
@@ -13,5 +13,8 @@
     // Autofocus first field with an error. (usability)
     var error_input;
     if (error_input = $('.has-error :input').first()) { error_input.focus(); }
+
+    // disable autocomplete globally
+    $('input[type="text"]').attr('autocomplete', 'off');
   });
 }(window.jQuery);
