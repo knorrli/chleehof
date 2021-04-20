5f482f3366c2c7c6cddeabd469c0ddb6c2ec20a9
Simon Kiener <jugglinghobo@gmail.com>
2021-04-20 23:10:20 +0200

    improved search (maybe?)

--- models/customer.rb
+++ models/customer.rb
@@ -4,9 +4,17 @@ class Customer < ActiveRecord::Base
   validates_presence_of :first_name, :last_name, unless: :company?
   validates_presence_of :address_1, :zip_code, :city
 
-  def self.search(query)
-    query = query.downcase
-    where('lower(company) LIKE ? OR lower(last_name) LIKE ? OR lower(first_name) LIKE ? OR lower(zip_code) LIKE ? OR lower(city) LIKE ?', "%#{query}%", "%#{query}%", "%#{query}%", "#{query}%", "%#{query}%")
+  def self.search(search)
+    search = search.downcase
+    searches = search.split(' ').map { |word| "#{word}%" }
+    query = 'lower(company) LIKE ANY(ARRAY[:searches]) OR lower(last_name) LIKE ANY(ARRAY[:searches]) OR lower(first_name) LIKE ANY(ARRAY[:searches])'
+    found_by_name = where(query, { searches: searches } )
+    if found_by_name.any?
+      found_by_name
+    else
+      query += 'OR lower(address_1) LIKE ANY(ARRAY[:searches]) OR lower(zip_code) LIKE ANY(ARRAY[:searches]) OR lower(city) LIKE ANY(ARRAY[:searches])'
+      where(query, { searches: searches })
+    end
   end
 
   def self.newest_customers
